23ee6e8a1821a9d2e978fed1304a19424bc06540
Shreyansh Misra <99529803+TangledDaunT@users.noreply.github.com>
2026-06-21 04:10:02 +0530

    feat: add swipeable control pages shell

---
+++ Phone App/lib/screens/control_pages_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'control_screen.dart';
+import 'mac_control_screen.dart';
+
+class ControlPagesScreen extends StatelessWidget {
+  const ControlPagesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const BouncingScrollPhysics(),
+      reverse: true,
+      children: const [
+        ControlScreen(),
+        MacControlScreen(),
+      ],
+    );
+  }
+}
